0a0b3acfeb22475101d7463e1d2628ad4ea48e9a
Noemiegrau <noemiegrau@outlook.fr>
2021-10-26 09:12:07 -0700

    styled skills section

--- src/blocks/page-title/PageTitleAbout.js
+++ src/blocks/page-title/PageTitleAbout.js
@@ -3,10 +3,10 @@ import React from 'react';
 const PageTitleAbout = () => {
     return (
         <section id="page-title">
-            <div id="aboutContainer" className="wrapper">
+            <div className="wrapper">
                 <div className="title position-relative">
                     {/* <h1>About me<span className="dot">.</span></h1> */}
-                    <h2 className="transform-scale-h"><span className="line">About me</span></h2>
+                    <h2 id="aboutContainer" className="transform-scale-h"><span className="line">About me</span></h2>
                     {/* <span className="dot">.</span> */}
                     {/* <div className="title-clone">About.</div> */}
                 </div>
--- src/nav/navLinks.js
+++ src/nav/navLinks.js
@@ -1,5 +1,5 @@
 export const navLinks = [
-	{ navLinkId: 'Home', scrollToId: 'header' },
+	{ navLinkId: 'Home', scrollToId: 'homehello' },
 	{ navLinkId: 'About', scrollToId: 'aboutContainer' },
 	{ navLinkId: 'Resume', scrollToId: 'resumeContainer' },
     { navLinkId: 'Work', scrollToId: 'my-works' },
