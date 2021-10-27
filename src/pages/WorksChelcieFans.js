81f9477ba3e46edacc0cbaadf17ee5e90d121e58
Noemiegrau <noemiegrau@outlook.fr>
2021-10-27 11:16:02 -0700

    update content of all projects pages and added some images

--- src/pages/WorksChelcieFans.js
+++ src/pages/WorksChelcieFans.js
@@ -38,19 +38,19 @@ const WorksChelcieFans = () => {
                                 <div className="col-xl-7 col-lg-7 col-md-7 col-sm-12">
                                     <div className="img object-fit">
                                         <div className="object-fit-cover">
-                                            <img src="assets/img/placeholder/966x560.jpg" alt="Product photography" />
+                                            <img src="assets/img/placeholder/works-inside/chelciefans.png" alt="Product photography" />
                                         </div>
                                     </div>
 
                                     <div className="img object-fit">
                                         <div className="object-fit-cover">
-                                            <img src="assets/img/placeholder/966x560.jpg" alt="Product photography" />
+                                            <img src="assets/img/placeholder/works-inside/chelciefans2.png" alt="Product photography" />
                                         </div>
                                     </div>
 
                                     <div className="img object-fit">
                                         <div className="object-fit-cover">
-                                            <img src="assets/img/placeholder/966x560.jpg" alt="Product photography" />
+                                            <img src="assets/img/placeholder/works-inside/chelciefans3.png" alt="Product photography" />
                                         </div>
                                     </div>
                                 </div>
@@ -64,14 +64,14 @@ const WorksChelcieFans = () => {
 
                                     <div className="description">
 
-                                        <p>This <strong>MERN app</strong> is a fan page website where users can create an account, post their thoughts for others to see, interact with other users through these posts, be friends with each other and comment on posts. This application also uses front-end routing, api requests (queries and mutations) and user authentication.
+                                        <p>This <strong>MERN app</strong> is a fan page website where users can <strong>create an account</strong>, post their thoughts for others to see, interact with other users through these posts, be friends with each other and <strong>comment on posts</strong>. This application also uses front-end routing, api requests (queries and mutations) and user authentication.
 </p>
 
                                         <h5>Tools</h5>
 
                                         <ul>
                                             <li>HTML5, CSS3, Javascript</li>
-                                            <li>MERN stack (MongoDB, Mongoose, Expressjs, REACT, Nodejs)</li>
+                                            <li><strong>MERN stack</strong> (MongoDB, Mongoose, Expressjs, REACT, Nodejs)</li>
                                             <li>GraphQl, Apollo, JsonWebToken</li>
                                             <li>Heroku, and more</li>
                                         </ul>
@@ -80,41 +80,23 @@ const WorksChelcieFans = () => {
 
                                         <ul>
 
-                                    <div className="list-group list-group-horizontal-sm">
-                                        <div className="list-group-item transform-scale-h">
-                                            <h4><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                                            </svg>  <a href="https://secret-hollows-41476.herokuapp.com/" target="_blank">See Deployed App</a></h4>
+                                    
+                                        <div className="transform-scale-h">
+                                        {/* <div>Icons made by <a href="https://www.flaticon.com/authors/fauzidea" title="FauzIDEA">FauzIDEA</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+                                            <h4>  <img src="assets/img/placeholder/atom.png" alt="" height="28" width="28" /> <a href="https://secret-hollows-41476.herokuapp.com/" target="_blank">See Deployed App</a></h4>
+                                        </div> <br />
 
-                                            {/* <p>Los Angeles, CA, USA</p> */}
-                                        </div>
-
-                                        <div className="list-group-item transform-scale-h">
-                                            <h4><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                                        <div className="transform-scale-h">
+                                            <h4><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                                             </svg>  <a href="https://github.com/simmonsW/chelcie-fans" target="_blank">See GitHub</a></h4>
-
-                                            {/* <p><a title="719-338-4628" href="tel:719-338-4628">213-458-1815</a></p> */}
                                         </div>
-                                    </div>
+                                    
 
 
                                         </ul>
 
                                     </div>
-
-                                    <br /><br /> <p>
-
-📖 
-📌
-🛠️ 
-🤝
-
-🔗 Links:
-GitHub repository: https://github.com/simmonsW/chelcie-fans
-Deployed App: https://secret-hollows-41476.herokuapp.com/ </p>
-
-
                                 </div>
                             </div>
                         </div>
