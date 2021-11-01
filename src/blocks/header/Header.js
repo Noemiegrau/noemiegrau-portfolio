3616d2c7e4abb20643899d94ab8b075a9ec0910f
Noemiegrau <noemiegrau@outlook.fr>
2021-11-01 11:16:29 -0700

    try to deploy again

--- src/blocks/header/Header.js
+++ src/blocks/header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 </div>
 
                 <div className="justify-content-end ml-auto">
-                    <Nav />
+                    {/* <Nav /> */}
                 </div>
             </div>
         </header>
